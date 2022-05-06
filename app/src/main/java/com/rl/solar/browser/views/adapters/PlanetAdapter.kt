608c6e34e310656be845939017d69919f80de534
Rob Lankey <rob.lankey@gmail.com>
2022-05-05 22:23:14 -0400

    flow updates & onItemClicked listener passed into adapter

--- app/src/main/java/com/rl/solar/browser/views/adapters/PlanetAdapter.kt
+++ app/src/main/java/com/rl/solar/browser/views/adapters/PlanetAdapter.kt
@@ -7,29 +7,40 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.rl.solar.core.Planet
 import com.rl.solar.databinding.ListItemPlanetBinding
-import javax.inject.Inject
 
-class PlanetAdapter @Inject constructor() : ListAdapter<Planet, RecyclerView.ViewHolder>(
-  PlanetDiffCallback()
-) {
+class PlanetAdapter(
+  val onItemClicked: (item: Planet) -> Unit
+) : ListAdapter<Planet, RecyclerView.ViewHolder>(PlanetDiffCallback()) {
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-    return PlanetViewHolder(
-      ListItemPlanetBinding.inflate(
-        LayoutInflater.from(parent.context),
-        parent,
-        false
-      )
-    )
+    val binding = ListItemPlanetBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    return PlanetViewHolder(binding)
   }
 
   override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-    val planet = getItem(position)
-    (holder as PlanetViewHolder).bind(planet)
+    getItem(position)?.let { planet ->
+      (holder as PlanetViewHolder).bind(planet)
+    }
   }
 
-  class PlanetViewHolder(
+  inner class PlanetViewHolder(
     private val binding: ListItemPlanetBinding
   ) : RecyclerView.ViewHolder(binding.root) {
+    init {
+      binding.planetListCard.setOnClickListener {
+        binding.planet?.let { planet ->
+          onItemClicked(planet)
+        }
+      }
+
+      binding.planetItemImage.setOnClickListener {
+        binding.planetListCard.performClick()
+      }
+
+      binding.plantItemTitle.setOnClickListener {
+        binding.planetListCard.performClick()
+      }
+    }
+
     fun bind(item: Planet) {
       binding.apply {
         planet = item
